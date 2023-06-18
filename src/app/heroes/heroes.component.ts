3ccdc1cf0b39bae05ce80a86a122d0b05e9bfedb
achyutjagini <achyut.jagini@gmail.com>
2023-06-18 17:23:01 +0530

    a

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -19,4 +20,9 @@ export class HeroesComponent {
     name: 'Windstorm'
   };*/
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
